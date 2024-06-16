c2d22d32c96f02ae3e8495f6fa3e9b927c607975
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-06-16 02:33:33 +0000

    Initial Commit

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,44 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        add user
+        """
+        if not email or not hashed_password:
+            return
+
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        self._session.add(new_user)
+        self.commit()
+        return new_user
+
