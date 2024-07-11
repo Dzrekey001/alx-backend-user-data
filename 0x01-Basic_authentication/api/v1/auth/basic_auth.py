a1077215dee736e39c5f86a6e21d1a92d68bafe9
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-11 13:51:10 +0000

    Implemented user_object_from_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Inherits from Auth to create a basic Auth"""
-from api.v1.auth.auth import Auth
+from api.v1.auth.auth import Authi
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -70,3 +72,28 @@ class BasicAuth(Auth):
             return name, pwd
         except Exception as e:
             return (None, None)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Fetches the user object based on email and password.
+
+        Args:
+            user_email (str): The user's email.
+            user_pwd (str): The user's password.
+
+        Returns:
+            Optional[User]: The user object if credentials are valid,
+            otherwise None.
+        """
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = user.search({"user_email": user_email})
+        if users:
+            for user_obj in users:
+                if user_obj.is_valid_password(pwd=user_pwd):
+                    return user_obj
+        return None
