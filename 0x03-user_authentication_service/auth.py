8b7cfd9a02ab184fc68fc15c18142553f1cfd0f3
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-16 11:34:25 +0000

    implemented the register_user method in auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
 import bcrypt
+from user import User
+from db import DB
+from typing import TypeVar
 
 
 def _hash_password(password: str) -> bytes:
     """returns a bytes that is a salted hash of the input password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode("utf-8"), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Hash the password using _hash_password,and save
+        the user to the database
+        Return:
+            ValueError if user already exist base on email.
+            Return user object on success.
+        """
+        if self._db.find_user_by(email=email):
+            raise ValueError(f"User {email} already exists")
+        hash_pwd = _hash_password(password)
+        return self._db.add_user(email, hash_pwd)
