df1fbc244410a35c87ace09bc02b8eaccd7617ad
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-16 12:32:25 +0000

    Implemented the users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -9,6 +13,17 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
