9a35862320c3cbd39af95f12530a04af866c485a
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-17 11:22:41 +0000

    Implementd the destroy session method in the auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,7 +91,7 @@ class Auth:
             None
         """
         try:
-            user = _db.find_user_by(id=user_id)
+            user = self._db.find_user_by(id=user_id)
             if user.session_id is not None:
                 self._db.update_user(user.id, session_id=None)
         except Exception:
