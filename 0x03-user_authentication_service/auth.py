d18549e350908d2a57e1dd3795a26e88866d7ecb
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-17 13:54:51 +0000

    edited code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -125,8 +125,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_pwd = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_pwd)
+            h_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=h_pwd, reset_token=None)
         except Exception:
             raise ValueError
         return None
