3962cc2898cb896ac5ea314546d152beb1a40053
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-17 13:57:14 +0000

    edited code error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -125,8 +125,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            h_pwd = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=h_pwd, reset_token=None)
-        except Exception:
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pwd,
+                                 reset_token=None)
+        except NoRultFound:
             raise ValueError
         return None
