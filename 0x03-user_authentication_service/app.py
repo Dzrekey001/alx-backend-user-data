f7ab2b8b8dc7107b18631fa5ad83be8bd8931dea
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-17 12:10:04 +0000

    edited some logical errors

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,16 +44,13 @@ def login():
 def logout():
     session_id = request.cookies.get("session_id")
     if session_id:
-        try:
-            user = AUTH.get_user_from_session_id(session_id)
-            if user is not None:
-                AUTH.destroy_session(user.id)
-            else:
-                raise ValueError
-        except Exception:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user is not None:
+            AUTH.destroy_session(user.id)
+            redirect(url_for("home"))
+        else:
             abort(403)
-        redirect(url_for("home"))
-    abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
