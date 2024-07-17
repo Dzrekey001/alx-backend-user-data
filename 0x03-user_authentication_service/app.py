4751b9bfa220f873dedf9a87cd80157b1c54b2de
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-07-17 14:48:33 +0000

    edited app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,16 +73,16 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'])
 def update_password():
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-
+    """ updates the user password """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
-    except Exception:
+        return jsonify({"email": email, "message": "Password updated"})
+    except Exception as e:
         abort(403)
 
 
